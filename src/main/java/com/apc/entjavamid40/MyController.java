c9862218e0a72448540f710261e9bf6f00004b80
Chancellor Galiza <chancellor.galiza@upd.edu.ph>
2024-09-21 09:46:07 +0800

    added the sample endpoint usage

--- src/main/java/com/apc/entjavamid40/MyController.java
+++ src/main/java/com/apc/entjavamid40/MyController.java
@@ -5,12 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("convert")
 public class MyController {
+
+    /*
+    * /convert/in/cm/1
+    *
+    * out put should be found in sample_out.json
+    * */
 
     @GetMapping("/convert/{unit1}/{unit2}/{value}")
     public String convert() {
         return "";
     }
+
+    List<String> allowedUnits = Arrays.asList("mm","cm","in","m","ft");
+
 }
